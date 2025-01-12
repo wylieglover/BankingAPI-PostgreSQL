d3c49b881ad780874c03969cbebfff125e33ced3
wylieglover <rg11445@georgiasouthern.edu>
2025-01-12 14:28:06 -0500

    Remove legacy JavaScript files and unused configurations

--- src/server/prisma/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.transaction_type = exports.account_type = exports.prisma = void 0;
-const client_1 = require("@prisma/client");
-Object.defineProperty(exports, "account_type", { enumerable: true, get: function () { return client_1.account_type; } });
-Object.defineProperty(exports, "transaction_type", { enumerable: true, get: function () { return client_1.transaction_type; } });
-const prisma = new client_1.PrismaClient();
-exports.prisma = prisma;
